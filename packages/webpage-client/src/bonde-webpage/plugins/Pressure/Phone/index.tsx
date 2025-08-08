c40f151d9aa142b2bbb08dc3cd6b70a4307259fc
Igor Santos <igr.exe@gmail.com>
2025-08-08 16:50:03 -0300

    fix(webpage): change targets to get in widget.settings

--- packages/webpage-client/src/bonde-webpage/plugins/Pressure/Phone/index.tsx
+++ packages/webpage-client/src/bonde-webpage/plugins/Pressure/Phone/index.tsx
@@ -12,6 +12,8 @@ type Target = {
 type Settings = {
   main_color?: string;
   title_text?: string;
+  call_script?: string;
+  targets?: Target[];
 }
 
 type Widget = {
@@ -21,27 +23,16 @@ type Widget = {
 
 type Props = {
   widget: Widget
-  asyncFetchTargets: ({ widget_id }: { widget_id: number }) => Promise<void>
 }
 
-export default function PhoneWidget({ widget, asyncFetchTargets }: Props) {
-  const [targets, setTargets] = useState<Target[]>([])
+export default function PhoneWidget({ widget }: Props) {
 
   const {
     main_color: mainColor,
     title_text: titleText,
+    call_script: guideline,
+    targets = []
   } = widget.settings || {};
-
-  // const 
-  // console.log("{widget}", {widget});
-
-  useEffect(() => {
-    asyncFetchTargets({ widget_id: widget.id })
-      .then(({ data }: any) => {
-        // console.log("asyncFetchTargets", { data });
-        setTargets(data.pressure_targets[0].targets as Target[]);
-      })
-  }, [])
 
   return (
     <Styled>
@@ -67,7 +58,7 @@ export default function PhoneWidget({ widget, asyncFetchTargets }: Props) {
         widgetId={widget.id}
         action={defaultPhoneCall}
         mainColor={mainColor || "blue"}
-        guideline="Olá, meu nome é [seu nome]. Estou ligando para pedir que [nome do alvo] faça [ação solicitada]. Essa decisão é muito importante porque [insira argumento principal]. Contamos com o apoio de vocês!"
+        guideline={guideline || ""}
         targets={targets}
         onFinish={(state) => {
           console.log("onFinish", { state });
@@ -164,6 +155,11 @@ const Styled = styled.div`
         font-weight: 600;
         color: #aaa;
       }
+    }
+    
+    .bonde-action-guideline {
+      font-size: 16px;
+      color: #505050;
     }
     
     .bonde-action-field__error {
